c2fefd7dfbbee53870ef4b36903fab9d2121246c
harrytran998 <nhattq.coding@gmail.com>
2026-02-21 12:59:48 +0700

    test: fix auth service test layer composition

--- apps/server/tests/unit/auth.service.test.ts
+++ apps/server/tests/unit/auth.service.test.ts
@@ -4,13 +4,12 @@ import {
   AuthService,
   AuthServiceLive,
 } from '../../src/modules/auth/application/services/auth.service'
+import { ConfigLayer } from '../../src/layers/index'
 import { InvalidCredentialsError } from '../../src/modules/auth/domain/errors/auth.errors'
 import { DatabaseServiceLive } from '../../src/services/database.service'
-import { RedisServiceLive } from '../../src/services/redis.service'
-import { ConfigLayer } from '../../src/layers/index'
 
 describe('AuthService', () => {
-  const TestLayer = Layer.provide(AuthServiceLive, Layer.mergeAll(ConfigLayer, DatabaseServiceLive, RedisServiceLive))
+  const TestLayer = Layer.provide(AuthServiceLive, Layer.mergeAll(ConfigLayer, DatabaseServiceLive))
 
   const runTest = <A, E>(effect: Effect.Effect<A, E, AuthService>) =>
     Effect.runPromise(Effect.provide(effect, TestLayer) as Effect.Effect<A, E, never>)
